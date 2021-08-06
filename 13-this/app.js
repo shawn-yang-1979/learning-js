367d943f14479c1b8127341d3df352bd1262a8d8
shawn.yang <shawn.yang@idtechproducts.com>
2021-08-06 03:04:27 +0000

    Add more comments

--- 13-this/app.js
+++ 13-this/app.js
@@ -1,16 +1,17 @@
 // Reference: https://www.youtube.com/watch?v=gvicrj31JOM
-
+// this means the owner of the function
+// When a function is owned by an object we call it method
 var user = {
     firstName: "Shang-hua",
     lastName: "Yang",
     nickName: "Shawn",
-    fullName() { // method, not function, because it is a property of an object
+    fullName() { // method, because it is a property of an object
         console.log(this);
-        return this.firstName + " " + this.lastName; // this means the object when in method
+        return this.firstName + " " + this.lastName; // this means the object who owns the function
     },
     aka: function () { // also method
         console.log(this);
-        return this.fullName() + " (" + this.nickName + ")"; // this means the object when in method
+        return this.fullName() + " (" + this.nickName + ")"; // this means the object who owns the function
     }
 }
 
@@ -23,13 +24,15 @@ console.log(user.fullName());
 console.log(user.aka());
 console.log(user.greet());
 
-var rf = function regularFunction() { // function, not method, because it is not a property of an object
-    console.log(this); // in a function, this means the global object.
+// The owner of the function can be both an object and the global object.
+// The actual this object depends on how you use the function.
+var rf = function regularFunction() { // function, we can't call it method yet, because it is not a property of an object
+    console.log(this); // in a function, this means the global object, because the owner of the function is global object.
 }
 rf();
 
-user.rf = rf; // now it becomes an method, not function, what the fuck.
-user.rf();
+user.rf = rf; 
+user.rf(); // now it becomes an method, what the fuck?
 
 rf(); // now it becomes a function again, not a method, fucking weird.
 
@@ -41,6 +44,7 @@ function User(firstName) {
 new User("Shawn"); // if you use new, this, will become and empty object
 User("Shawn"); // if not using new, this will become the global object.
 
+// The owner of a callback function is the global object by default.
 var video1 = {
     tags: [1, 2, 3],
     showTags() {
